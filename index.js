c1408d5875f9dbe53bad7ea314768c148f1bd6c8
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-07-01 12:35:20 +0600

    get task

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
@@ -16,11 +17,8 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xquqwhb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const taskCollection = client.db("to-do-list").collection("task");
-  // perform actions on the collection object
-  client.close();
-});
+const taskCollection = client.db("to-do-list").collection("task");
+const taskCompletedCollection = client.db("to-do-list").collection("task-complete");
 
 async function run(){
     try{
@@ -28,8 +26,30 @@ async function run(){
 
         app.post('/task', async (req, res)=>{
             const task = req.body;
-            const result = await taskCollection.insertOne(task);
+            console.log(task)
+            // const quote = {task: task}
+            // console.log(quote)
+            const exist = await taskCollection.findOne(task)
+            if(exist){
+                return res.send({success: false, task: exist})
+            }
+                const result = await taskCollection.insertOne(task)
+                return res.send({success: true, result})            
+        })
+
+        app.get('/task', async(req, res)=>{
+            const result = await taskCollection.find().toArray();
             res.send(result)
+        })
+
+        app.post('/taskComplete', async (req, res)=>{
+            const id = req.body;
+            // console.log(id)
+            // const result = await taskCompletedCollection.insertOne(task)
+            // res.send(result)
+            const query = {_id: ObjectId(id)}
+            const task = await taskCollection.findOne(query)
+            console.log(task)
 
         })
 
